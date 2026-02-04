bd396b28e99ead7abe0f234c7fe7006abfeaaeef
Ayinde John <lolaayinde@gmail.com>
2026-02-04 14:17:44 +0100

    chore: initial project setup
    - Add package.json with dependencies
    - Configure TypeScript with strict mode
    - Set up Jest for testing
    - Add nodemon for development
    - Configure environment variables
    - Add MIT license

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/app.ts',
+    '!src/migrations/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  verbose: true,
+};
